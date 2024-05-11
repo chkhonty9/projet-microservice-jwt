9137ed5f4a7cb1998bdf591d345b9c081a7bf476
nouhaila <nohaila.chkhonty@gmail.com>
2024-05-11 22:40:57 +0100

    adding user and role in front

---
+++ angular-front-end/src/app/model/user.ts
@@ -0,0 +1,12 @@
+import {Role} from "./role";
+
+export interface User {
+  id: number,
+  username: string,
+  password: string,
+  name: string,
+  email: string,
+  address: string ,
+  phoneNumber: string ,
+  roles: Array<Role>
+}
